859ba93f970e2d1ffa6cfe75fe64e793ce23a837
Dileep <dileepmohanan@gmail.com>
2016-08-22 16:26:46 -0700

    Spring Security related files newly created

---
+++ src/main/java/com/att/cw/security/UserLogin.java
@@ -0,0 +1,18 @@
+package com.att.cw.security;
+
+public class UserLogin {
+    public String name;
+    public String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
